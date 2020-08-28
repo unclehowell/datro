3fcde00be6c40914763d2200cc334fb328943606
Sion <unclehowell>
2020-08-28 03:12:08 -0400

    keeps timing out so I trimmed the repo down a bit

--- static/datro/html/multistep-menu/run.sh
+++
@@ -1,6 +0,0 @@
-# clear the content of the web-server directory
-rm -r /var/www/html/index.html
-
-# move the content to the right directories
-mv /var/WaveOS/modeless/features/multistep-menu/* /var/www/html
-
