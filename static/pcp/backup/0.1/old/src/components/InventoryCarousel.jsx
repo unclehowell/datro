91fa0e7ee7c56988b37be0e58434765c19c43042
Sion Buckler <unclehowell@protonmail.com>
2026-02-20 05:11:20 -0300

    Auto-commit: 2026-02-20 05:11:17 - Automated PR update.

---
+++ static/pcp/backup/0.1/old/src/components/InventoryCarousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import BannerCard from './BannerCard.jsx'
+
+export default function InventoryCarousel({ items, onOpen }) {
+  return (
+    <div className="inventory-carousel">
+      {items.map((it) => (
+        <div key={it.id} className="inventory-slide">
+          <BannerCard
+            item={it}
+            onOpen={onOpen}
+            onDoubleOpen={onOpen}
+            footer={<div className="inventory-hint">Double-click to open dedicated ad page</div>}
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
